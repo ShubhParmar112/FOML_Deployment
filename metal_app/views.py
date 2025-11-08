c0a04f043b8acec255e61e9a434ec5c6207de4fb
ShubhParmar112 <shubh.parmar.999@gmail.com>
2025-11-08 08:14:50 +0530

    Initial Commit

---
+++ metal_app/views.py
@@ -0,0 +1,33 @@
+# metal_app/views.py
+from django.shortcuts import render
+from django.conf import settings
+from .forms import AudioUploadForm
+from .audio_processor import predict_metal_from_file
+import os
+
+def home(request):
+    form = AudioUploadForm()
+    result = None
+
+    if request.method == 'POST':
+        form = AudioUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            audio_file = request.FILES['audio_file']
+            file_path = os.path.join(settings.MEDIA_ROOT, 'uploads', 'temp_recording.wav')
+
+            # Save file
+            with open(file_path, 'wb+') as f:
+                for chunk in audio_file.chunks():
+                    f.write(chunk)
+
+            # Predict
+            result = predict_metal_from_file(file_path)
+
+            # Clean up
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
+    return render(request, 'index.html', {
+        'form': form,
+        'result': result
+    })
